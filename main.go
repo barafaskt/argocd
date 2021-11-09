1defe9940a295e93d8872c6e1c2a115a20843c9a
Rafael Pereira da Silva <barafaskt@gmail.com>
2021-11-08 22:27:34 -0300

    fix: alter name for ArgoCD deployment

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	var port string = ":8087"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello world!!! Latest</h1>"))
+		w.Write([]byte("<h1>Hello Rafael!!! ARGOCD</h1>"))
 	})
 	fmt.Printf("Web Service is Running. Port: %s", strings.Replace(port, ":", "", 1))
 	http.ListenAndServe(port, nil)
